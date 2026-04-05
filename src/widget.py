cbabd47011ac193d5d8f3c8b6a3fe940564a0252
Anna Kupriianova <bredbery4@gmail.com>
2026-04-05 16:59:20 +0300

    Add get_date function

--- src/widget.py
+++ src/widget.py
@@ -20,3 +20,17 @@ def mask_account_card(card_info: str) -> str:
         return f"{type_name} {get_mask_account(number)}"
     else:
         return f"{type_name} {get_mask_card_number(number)}"
+
+
+def get_date(date_raw: str) -> str:
+    """
+    Принимает строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "11.03.2024".
+    """
+    # Делим на части по индексам, извлекаем: день (8, 9), месяц (5, 6) и год (0, 1, 2, 3)
+    day = date_raw[8:10]
+    month = date_raw[5:7]
+    year = date_raw[0:4]
+
+    # Возвращаем дату в нужном формате
+    return f"{day}.{month}.{year}"
